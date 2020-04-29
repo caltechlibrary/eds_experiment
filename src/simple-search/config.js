7a7b833fb4f353592699bb5df21060fec42f36f5
Michael Hucka <mhucka@caltech.edu>
2020-04-29 11:09:03 -0700

    Restructure directory hierarchy

--- src/simple-search/config.js
+++ src/simple-search/config.js
@@ -1,12 +1,12 @@
 const config = {
-    "user"             : "",
-    "password"         : "",
+    "user"             : "v|~92ojb9mnq$bsjy8zn",
+    "password"         : "219qh6Tfl9CZr!MQTzv8",
     "authURL"          : 'https://eds-api.ebscohost.com/authservice/rest/uidauth',
     "sessionURL"       : 'https://eds-api.ebscohost.com/edsapi/rest/createsession',
     "searchURL"        : 'https://eds-api.ebscohost.com/edsapi/rest/Search',
     "corsproxy"        : {
-        "url": '',
-        "headers": { }
+        "url": 'http://synonym.caltech.edu:8090/',
+        "headers": {'X-Proxy-Cors': 'true' }
     },
 };
 
