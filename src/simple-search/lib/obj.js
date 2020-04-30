b09591f66d72b1715e8c8b2198e458b20cda69ae
Michael Hucka <mhucka@caltech.edu>
2020-04-30 14:15:28 -0700

    Start a file for object utilities

---
+++ src/simple-search/lib/obj.js
@@ -0,0 +1,20 @@
+// ============================================================================
+// @file    obj.js
+// @brief   JavaScript object utilities
+// @author  Michael Hucka <mhucka@caltech.edu>
+// @license Please see the file named LICENSE in the project directory
+// @website https://github.com/caltechlibrary/eds_experiment
+// ============================================================================
+
+var obj = {
+
+    // Based on https://stackoverflow.com/a/59787784/743730 posted by user
+    // Kamil Kiełczewski on 2020-01-17, this is the fastest implementation.
+    isEmpty: function isEmpty(thing) {
+        for (var name in thing)
+            return false;
+        return true;
+    },
+}
+
+module.exports = obj;
