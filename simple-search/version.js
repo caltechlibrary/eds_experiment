d2e80d2bedf16ff65d7ae0c047e7beb6466a9ed5
Michael Hucka <mhucka@caltech.edu>
2020-05-04 16:48:10 -0700

    Update version.js

--- simple-search/version.js
+++ simple-search/version.js
@@ -1,7 +1,7 @@
 const version = {
-    count: "96",
-    description: "1d93ee2-local",
-    string: "96 (1d93ee2-local)",
+    count: "97",
+    description: "6b6817b-local",
+    string: "97 (6b6817b-local)",
 }
 
 module.exports = version;
